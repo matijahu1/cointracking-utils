f400cba74c064014b629385dc242125006c3ac0e
matijahu1 <matijahu1@proton.me>
2025-12-21 22:37:13 +0100

    Skip tests if test data file is not provided

--- aggregation_tool/tests/test_cointracking_aggregator.py
+++ aggregation_tool/tests/test_cointracking_aggregator.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from aggregation_tool.tests.utils.aggregator_helpers import run_aggregator_test
@@ -24,7 +26,21 @@ def mock_config() -> MockConfig:
             "./aggregation_tool/data/CT-test-data3.csv",
             "./aggregation_tool/data/CT-test-data3-exp.csv",
         ),
+        (
+            "./aggregation_tool/data/CT-test-data4.csv",
+            "./aggregation_tool/data/CT-test-data4-exp.csv",
+        ),
     ],
 )
 def test_csv_data(input_file, expected_file, mock_config):
+    if not os.path.exists(input_file):
+        pytest.skip(
+            f"\n[INFO] Skipping test: {input_file} not found. Create local data to run this."
+        )
+
+    if not os.path.exists(expected_file):
+        pytest.skip(
+            f"\n[INFO] Skipping test: {expected_file} not found. Create local data to run this."
+        )
+
     run_aggregator_test(input_file, expected_file, mock_config)
